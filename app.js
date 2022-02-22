4764ca6b4ff7662118964f3edc80ae4ad50c3cde
NishatFariza <nishatfarzia78@gmail.com>
2022-02-22 13:14:10 +0600

    json intro

---
+++ app.js
@@ -0,0 +1,14 @@
+const user ={id:12, name:'Nishat', color:'Black'};
+// console.log(user);
+
+// const stringified = JSON.stringify(user);
+// console.log(stringified);
+
+const shop ={
+    name:'Nishat',
+    address: 'station road',
+    product: ['mobile, laptop'],
+    isExpensive : true
+}
+const stringified =JSON.stringify(shop);
+console.log(stringified)
